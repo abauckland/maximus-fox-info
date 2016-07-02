b7418e3c26b49bac5dfc5f413ffc2d7fe38bda02
abauckland <abauckland@gmail.com>
2016-07-02 16:08:45 +1200

    add devise 1

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
 
+  devise_for :users
+  
+  resources :guides
+  
   namespace :api, defaults: {format: 'json'} do
     namespace :v1 do
       resources :items, :only => [:show] do
