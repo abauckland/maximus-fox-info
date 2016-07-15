5b249531daba0c64106f8e05f849285de6e8be01
abauckland <abauckland@gmail.com>
2016-07-16 10:35:24 +1200

    product input update 23

--- test/models/product_test.rb
+++ test/models/product_test.rb
@@ -4,10 +4,8 @@ class ProductTest < ActiveSupport::TestCase
 #associations
    test "should have associations" do
      product = Product.new
-     assert_respond_to(product, :clauseproducts)
-     assert_respond_to(product, :productidentities)
-     assert_respond_to(product, :identities)
-     assert_respond_to(product, :instances)
+     assert_respond_to(product, :items)
+     assert_respond_to(product, :productkeywords)
      assert_respond_to(product, :item)
    end
 end
